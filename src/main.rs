9a9eba3e521242f2406b84ace237ba690d5248d4
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2024-05-01 21:59:12 -0400

    Shadowing Example

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,17 @@ fn main() {
     x = 6;
     println!("The new value of x is {}", x);
 
-    const THREE_HOURS_IN_SECONDS:u32 = 60*60*3;
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
 
-    println!("How many seconds in 3 hours? {THREE_HOURS_IN_SECONDS}")
+    println!("How many seconds in 3 hours? {THREE_HOURS_IN_SECONDS}");
+
+    let shadow_x = 18;
+
+    let shadow_x = shadow_x + 9;
+    {
+        let shadow_x = shadow_x * 2;
+        println!("The value of shadow_x in the inner scope is {shadow_x}");
+    }
+
+    println!("The value of shadow_x is {shadow_x}");
 }
