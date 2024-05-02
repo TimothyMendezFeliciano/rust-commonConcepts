77c435696b6e7941e273ec87088aa1009ac154c1
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2024-05-01 21:47:19 -0400

    Constants

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,8 @@ fn main() {
     // x is now mutable.
     x = 6;
     println!("The new value of x is {}", x);
+
+    const THREE_HOURS_IN_SECONDS:u32 = 60*60*3;
+
+    println!("How many seconds in 3 hours? {THREE_HOURS_IN_SECONDS}")
 }
