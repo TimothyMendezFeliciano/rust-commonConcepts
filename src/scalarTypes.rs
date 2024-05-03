208bc36f625ada6568b6fb2f50d4ddfb99eb1f04
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2024-05-03 14:43:58 -0400

    cleanup

--- src/scalarTypes.rs
+++ src/scalarTypes.rs
@@ -1,16 +1,16 @@
 fn scalarMain() {
-    let trueOrFalse: bool = false;
-    let signedNumber8Bit: u8 = 255;
-    let signedNumber16Bit: u16 = 2255;
-    let signedNumber32Bit: u32 = 2222555;
-    let signedNumber64Bit: u64 = 2222555;
-    let signedNumber128Bit: u128 = 22222255555;
-    let number8Bit: i8 = -128;
-    let number16Bit: i16 = -1128;
-    let number32Bit: i32 = -11228;
-    let number64Bit: i64 = -112288;
-    let number128Bit: i128 = -11228800;
+    let true_or_false: bool = false;
+    let signed_number_8Bit: u8 = 255;
+    let signed_number_16Bit: u16 = 2255;
+    let signed_number_32Bit: u32 = 2222555;
+    let signed_number_64Bit: u64 = 2222555;
+    let signed_number_128Bit: u128 = 22222255555;
+    let number_8Bit: i8 = -128;
+    let number_16Bit: i16 = -1128;
+    let number_32Bit: i32 = -11228;
+    let number_64Bit: i64 = -112288;
+    let number_128Bit: i128 = -11228800;
 
-    let floatinPointExample: f32 = 128.256;
-    let characterExample: char = 'x';
+    let floatin_point_example: f32 = 128.256;
+    let character_example: char = 'x';
 }
